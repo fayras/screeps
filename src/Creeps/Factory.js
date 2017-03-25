0206008a365a5c93a5a612031173adee031cd511
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2017-03-25 18:38:19 +0100

    CreepFactories bekommen nun den richtigen Spawn zugewiesen.

--- src/Creeps/Factory.js
+++ src/Creeps/Factory.js
@@ -1,6 +1,12 @@
 class Factory {
-  constructor() {
-
+  constructor(room) {
+    this.spawn = null;
+    this.room = room;
+    for(let spawnName in Game.spawns) {
+      if(room.name === Game.spawns[spawnName].room.name) {
+        this.spawn = Game.spawns[spawnName];
+      }
+    }
   }
 }
 
--- src/Creeps/Population.js
+++ src/Creeps/Population.js
@@ -3,7 +3,7 @@ const CreepFactory = require('Creeps.Factory');
 class Population {
   constructor(room) {
     this.room = room;
-    this.factory = new CreepFactory();
+    this.factory = new CreepFactory(room);
     this.creeps = this.room.find(FIND_MY_CREEPS);
   }
 }
